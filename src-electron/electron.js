4bd5acbff1524defd0ce50b0d0e81ffd76e26664
piyali2016 <piyali.acn@gmail.com>
2016-12-09 19:45:53 +0530

    9122016
    electron

---
+++ src-electron/electron.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow, globalShortcut } = require('electron');
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({ 
+        width: 800,
+        height: 700,
+        center: true,
+        minimizable: true,
+        alwaysOnTop: false,
+        autoHideMenuBar: true
+    });
+
+    win.loadURL(`file://${__dirname}/index.html`);
+
+    globalShortcut.register('CmdOrCtrl+Shift+D', ()=> {
+        win.webContents.toggleDevTools();
+    });
+    //client.create(win);
+    win.on('closed', () => {
+        win = null;
+    });
+}
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    globalShortcut.unregisterAll();
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+});
